f71d006ca4f90fdb5dfcabe95b2412ff829e67c7
lww <1013232063@qq.com>
2020-10-23 18:13:15 +0800

    hystrix

---
+++ cloud-provider-hystrix-payment8001/src/main/java/com/ww/springcloud/PaymentHystrixMain8001.java
@@ -0,0 +1,20 @@
+package com.ww.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author linweiwei
+ * @version 1.0
+ * @date 2020-10-23 18:05
+ * @describe:
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+public class PaymentHystrixMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentHystrixMain8001.class, args);
+    }
+}
